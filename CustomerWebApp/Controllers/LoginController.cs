1f1e5f44440494b7c9f8d1514b5aeca5d6e64d9a
Andreas Garvik <andreas.garvik13@gmail.com>
2020-02-27 23:01:02 +0100

    :sparkles: Added routing and db

--- CustomerWebApp/Controllers/LoginController.cs
+++ CustomerWebApp/Controllers/LoginController.cs
@@ -1,27 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using CustomerWebApp.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CustomerWebApp.Controllers
 {
     [ApiController]
-    [Route("api/login")]
+    [Route("api/[controller]")]
     public class LoginController : ControllerBase
     {
         private readonly ILogger<LoginController> _logger;
+        private readonly DatabaseContext db;
+        private int usernr = 1;
 
-        public LoginController(ILogger<LoginController> logger)
+        public LoginController(ILogger<LoginController> logger, DatabaseContext context)
         {
+            db = context;
             _logger = logger;
         }
 
         [HttpPost]
         public User Post(User user)
         {
-            // try to find user with this email in database, return if not create new
-            return new User { Usernr = 123, Email = user.Email, Password = user.Password };
+            List<User> ul = db.Users.ToList().Where(u => u.Email == user.Email).ToList();
+            if (ul.Count == 0)
+            {
+                User u = new User { Usernr = usernr++, Email = user.Email, Password = user.Password };
+                db.Users.Add(u);
+                db.SaveChanges();
+                return u;
+            }
+            else
+            {
+                return ul.First();
+            }
         }
 
     }
--- CustomerWebApp/Database/DatabaseContext.cs
+++ CustomerWebApp/Database/DatabaseContext.cs
@@ -12,14 +12,12 @@ namespace CustomerWebApp.Database
         public DbSet<User> Users { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
 
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Seed();
         }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql("Server=data1.hib.no; Port=5432; Database=h571556;UserId=h571556;Password=pass; Integrated Security = true;");
-
     }
 }
 
